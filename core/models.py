c0a5d1e06124f1495cec59e4ad797a34b3d9a432
Anteconix <magnofilho2323@gmail.com>
2022-08-29 06:11:42 -0300

    a

--- core/models.py
+++ core/models.py
@@ -2,21 +2,21 @@ import email
 from django.db import models
 
 class Usuario(models.Model):
-    email = models.CharField(max_length=100, notnull=True)
-    dt_nasc_usuario = models.DateField(notnull=True)
+    email = models.CharField(max_length=100)
+    dt_nasc_usuario = models.DateField()
     id_usuario = models.PrimaryKey(auto_created=True, primary_key=True)
-    nome_usuario = models.CharField(max_length=100, notnull=True)
-    senha = models.CharField(max_length=100, notnull=True)
+    nome_usuario = models.CharField(max_length=100)
+    senha = models.CharField(max_length=100)
 
 class Artista(models.Model):
-    nome_artista = models.CharField(max_length=100, notnull=True)
+    nome_artista = models.CharField(max_length=100)
     id_artista = models.PrimaryKey(auto_created=True, primary_key=True)
-    dt_nasc_artista = models.DateTimeField(notnull=True)
+    dt_nasc_artista = models.DateTimeField()
     biografia = models.TextField(max_length=2500)
 
 class Album(models.Model):
-    nome_album = models.CharField(max_length=100, notnull=True)
+    nome_album = models.CharField(max_length=100)
     id_album = models.PrimaryKey(auto_created=True, primary_key=True)
-    numero_musicas = models.IntegerField(max_length=2, notnull=True)
-    capa_album = models.BinaryField(binary=True,notnull=True)
+    numero_musicas = models.IntegerField(max_length=2)
+    capa_album = models.BinaryField(binary=True)
     
