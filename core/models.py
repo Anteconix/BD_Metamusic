af2926f771c76022278e35c87670d8aac7c4a5eb
Anteconix <magnofilho2323@gmail.com>
2022-08-29 05:44:31 -0300

    a

--- core/models.py
+++ core/models.py
@@ -1,4 +1,8 @@
+import email
 from django.db import models
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+class Usuario(models.Model):
+    email = models.CharField(max_length=100, notnull=True)
+    dt_nasc_usuario = models.DateField()
+    id_usuario = models.PrimaryKey( auto_created=True, primary_key=True)
+    nome_usuario = models.CharField(max_length=100, notnull=True)
