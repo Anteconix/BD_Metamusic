2de0a6a4106ce1724f7ebd6db01282a1e318a790
Anteconix <magnofilho2323@gmail.com>
2022-08-29 05:49:22 -0300

    a

--- core/models.py
+++ core/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Usuario(models.Model):
     email = models.CharField(max_length=100, notnull=True)
-    dt_nasc_usuario = models.DateField()
+    dt_nasc_usuario = models.DateField(notnull=True)
     id_usuario = models.PrimaryKey( auto_created=True, primary_key=True)
     nome_usuario = models.CharField(max_length=100, notnull=True)
+    senha = models.CharField(max_length=100, notnull=True)
+
+class Artista(models.Model):
+    
