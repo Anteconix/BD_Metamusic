fc221d75c5d945118a344b01e5da16b1473e58c8
AmandaCrispim <amandacrispim384@gmail.com>
2022-08-30 14:07:34 -0300

    a

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 import email
 from django.db import models
 
+
 class Usuario(models.Model):
     email = models.CharField(max_length=100)
     dt_nasc_usuario = models.DateField()
@@ -8,11 +9,13 @@ class Usuario(models.Model):
     nome_usuario = models.CharField(max_length=100)
     senha = models.CharField(max_length=100)
 
+
 class Artista(models.Model):
     nome_artista = models.CharField(max_length=100)
     id_artista = models.IntegerField(auto_created=True, primary_key=True)
     dt_nasc_artista = models.DateTimeField()
     biografia = models.TextField(max_length=2500)
+
 
 class Album(models.Model):
     nome_album = models.CharField(max_length=100)
@@ -20,10 +23,12 @@ class Album(models.Model):
     numero_musicas = models.IntegerField(max_length=2)
     data = models.TextField(db_column='data', blank=True)
 
+
 class Noticias(models.Model):
     titulo_noticias = models.CharField(max_length=255)
     conteudo_noticia = models.TextField(max_length=255)
     id_noticia = models.IntegerField(auto_created=True, primary_key=True)
+
 
 class Banda_Grupo(models.Model):
     integrantes = models.CharField(max_length=255)
@@ -32,7 +37,8 @@ class Banda_Grupo(models.Model):
     desc_banda = models.TextField(max_length=2500)
     nome_banda = models.CharField(max_length=100)
 
+
 class Musica(models.Model):
-        titulo_musica = models.CharField(max_length=255)
-        id_musica = models.IntegerField(auto_created=True, primary_key=True)
-        tempo_musica = models.TimeField()
+    titulo_musica = models.CharField(max_length=255)
+    id_musica = models.IntegerField(auto_created=True, primary_key=True)
+    tempo_musica = models.TimeField()
---
+++ core/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Categoria, Editora
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
