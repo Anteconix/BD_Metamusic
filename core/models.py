5912a34784e533e59bf68e631647f468a2944cf3
Anteconix <magnofilho2323@gmail.com>
2022-08-30 14:46:10 -0300

    a

--- core/models.py
+++ core/models.py
@@ -1,4 +1,3 @@
-import email
 from django.db import models
 
 
--- core/views.py
+++ core/views.py
@@ -1,29 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Usuario, Artista, Album, Noticias, Banda_Grupo, Musica
+
 from core.serializers import UsuarioSerializer, ArtistaSerializer, AlbumSerializer, NoticiasSerializer, Banda_GrupoSerializer, MusicaSerializer
 
-class UsuarioSerializer(ModelViewSet):
+class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
 
-class ArtistaSerializer(ModelViewSet):
+class ArtistaViewSet(ModelViewSet):
     queryset = Artista.objects.all()
     serializer_class = ArtistaSerializer
 
-class AlbumSerializer(ModelViewSet):
+class AlbumViewSet(ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
-class NoticiasSerializer(ModelViewSet):
+class NoticiasViewSet(ModelViewSet):
     queryset = Noticias.objects.all()
     serializer_class = NoticiasSerializer
 
-class Banda_GrupoSerializer(ModelViewSet):
+class Banda_GrupoViewSet(ModelViewSet):
     queryset = Banda_Grupo.objects.all()
     serializer_class = Banda_GrupoSerializer
 
-class MusicaSerializer(ModelViewSet):
+class MusicaViewSet(ModelViewSet):
     queryset = Musica.objects.all()
     serializer_class = MusicaSerializer
