b418a0b2aa1d5522c7f3fce055c172b1ae3b3544
Maria Laura Viana <marialauraviana@hotmail.com>
2022-09-05 09:28:34 -0300

    05/09 9:28

--- core/models.py
+++ core/models.py
@@ -2,13 +2,13 @@ from pyexpat import model
 from django.db import models
 
 
+
 class Usuario(models.Model):
     email = models.CharField(max_length=100)
     dt_nasc_usuario = models.DateField()
     id_usuario = models.IntegerField(auto_created=True, primary_key=True)
     nome_usuario = models.CharField(max_length=100)
     senha = models.CharField(max_length=100)
-
 
 class Artista(models.Model):
     nome_artista = models.CharField(max_length=100)
@@ -23,12 +23,10 @@ class Album(models.Model):
     numero_musicas = models.IntegerField()
     data = models.TextField(db_column='data', blank=True)
 
-
 class Noticias(models.Model):
     titulo_noticias = models.CharField(max_length=255)
     conteudo_noticia = models.TextField(max_length=255)
     id_noticia = models.IntegerField(auto_created=True, primary_key=True)
-
 
 class Banda_Grupo(models.Model):
     integrantes = models.CharField(max_length=255)
@@ -36,7 +34,6 @@ class Banda_Grupo(models.Model):
     dt_criacao = models.DateField()
     desc_banda = models.TextField(max_length=2500)
     nome_banda = models.CharField(max_length=100)
-
 
 class Musica(models.Model):
     titulo_musica = models.CharField(max_length=255)
