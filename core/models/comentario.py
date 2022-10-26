88e01d3e9d6ee902a6e0890451cb0e2072395687
Thiago Draeger <thiagodraeger123@gmail.com>
2022-10-26 16:20:51 -0300

    autonow

--- core/models/comentario.py
+++ core/models/comentario.py
@@ -3,8 +3,8 @@ from .usuario import Usuario
 
 class Comentario(models.Model):
     usuario = Usuario
-    data_comentario = models.DateField()
+    data_comentario = models.DateField(auto_now_add=True)
     comentario = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.nome_comentario
+        return self.comentario
