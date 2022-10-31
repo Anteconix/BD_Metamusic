68f9077941b9ac3b87f4a00c5a591480434d0ef3
Anteconix <magnofilho2323@gmail.com>
2022-10-31 09:22:15 -0300

    PyhtonAnywhere

--- metamusic/settings.py
+++ metamusic/settings.py
@@ -2,10 +2,10 @@ import environ
 import os
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env()
 environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
-
-BASE_DIR = Path(__file__).resolve().parent.parent
  
 SECRET_KEY = env('SECRET_KEY')
 
