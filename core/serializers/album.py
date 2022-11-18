81b9a495c03705bb259c91c9039cc933d40c280a
Thiago Draeger <thiagodraeger123@gmail.com>
2022-11-18 14:34:15 -0300

    album serializer

--- core/serializers/album.py
+++ core/serializers/album.py
@@ -1,7 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from core.models import Album
-
+from media.models import Image
+from media.serializers import ImageSerializer
+ 
 class AlbumSerializer(ModelSerializer):
-    class Meta:
-        model = Album
-        fields = "__all__"
+   class Meta:
+       model = Album
+       fields = "__all__"
+   foto_attachment_key = SlugRelatedField(
+       source="foto",
+       queryset=Image.objects.all(),
+       slug_field="attachment_key",
+       required=False,
+       write_only=True,
+   )
+   foto = ImageSerializer(required=False, read_only=True)   
+   capa_album_attachment_key = SlugRelatedField(
+       source="capa_album",
+       queryset=Image.objects.all(),
+       slug_field="attachment_key",
+       required=False,
+       write_only=True,
+   )
+   capa_album = ImageSerializer(required=False, read_only=True)   
