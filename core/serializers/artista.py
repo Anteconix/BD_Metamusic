5c3a6e2a9cd67ca052480850f901e473113a3709
Thiago Draeger <89817787+thiagodraeger@users.noreply.github.com>
2022-12-03 11:26:09 -0300

    Update artista.py

--- core/serializers/artista.py
+++ core/serializers/artista.py
@@ -1,7 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from core.models import Artista
+from media.models import Image
+from media.serializers import ImageSerializer
 
 class ArtistaSerializer(ModelSerializer):
     class Meta:
         model = Artista
         fields = "__all__"
+    foto_artista_attachment_key = SlugRelatedField(
+        source="foto_artista",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    foto_artista = ImageSerializer(required=False, read_only=True) 
