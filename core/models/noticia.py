2a1fc07e4ca1c0672977ae7d8d3192bce16b5916
= <=>
2022-12-06 13:52:15 -0300

    comentario e subtitulo

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -3,6 +3,7 @@ from django.db import models
 class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
     dt_noticia = models.DateField(auto_now_add=True)
+    subtitulo = models.CharField(max_length=255)
     conteudo_noticia = models.TextField(max_length=255)
 
     def __str__(self):
