4e19c4274afd2000ec9979c0f8eb3d1a618d7f6f
Anteconix <magnofilho2323@gmail.com>
2022-12-06 14:23:53 -0300

    corpo de texto notícia

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -4,7 +4,11 @@ class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
     dt_noticia = models.DateField(auto_now_add=True)
     subtitulo = models.CharField(max_length=255)
-    conteudo_noticia = models.TextField(max_length=255)
+    corpo1 = models.TextField(max_length=255)
+    corpo2 = models.TextField(max_length=255)
+    corpo3 = models.TextField(max_length=255)
+    corpo4 = models.TextField(max_length=255)
+    corpo5 = models.TextField(max_length=255)
 
     def __str__(self):
         return self.titulo_noticia
