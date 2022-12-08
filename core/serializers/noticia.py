858e5831c4091611e3e7418a6f75ee99dcae5dcb
Anteconix <magnofilho2323@gmail.com>
2022-12-08 13:01:49 -0300

    slug

--- core/serializers/noticia.py
+++ core/serializers/noticia.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from core.models import Noticia
 
 class NoticiaSerializer(ModelSerializer):
