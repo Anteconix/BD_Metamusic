3e1d0f1805dc683fd06a80902cdbaefef12365ac
Anteconix <magnofilho2323@gmail.com>
2022-12-08 11:48:01 -0300

    foto noticia fix

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.titulo_noticia
     dt_noticia = models.DateField(auto_now_add=True)
     subtitulo = models.CharField(max_length=255, null=True)
     texto = models.TextField(null=True)
@@ -14,6 +17,5 @@ class Noticia(models.Model):
         default=None,
     )
 
-
     def __str__(self):
-        return self.titulo_noticia
+        return self.nome_banda
