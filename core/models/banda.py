e54958b8b5826041882656c676dccddc244e0617
Anteconix <magnofilho2323@gmail.com>
2022-12-08 10:13:31 -0300

    noticia fix

--- core/models/banda.py
+++ core/models/banda.py
@@ -3,9 +3,7 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 from media.models import Image
 from .artista import Artista
-
 from media.models import Image
-
 class Banda(models.Model):
     nome_banda = models.CharField(max_length=100)
     ano_criacao = models.CharField(max_length=4)
@@ -24,7 +22,6 @@ class Banda(models.Model):
 
     def __str__(self):
         return self.nome_banda
-
     capa_banda = models.ForeignKey(
         Image,
         related_name="+",
