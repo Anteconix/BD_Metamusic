b0b2362ceec1bb9076ac8fd0d5f1f64adda33df1
Anteconix <magnofilho2323@gmail.com>
2022-12-08 11:53:41 -0300

    noiticia imagem fix

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -1,7 +1,5 @@
 from django.db import models
 from media.models import Image
-from media.models import Image
-
 
 class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
@@ -11,14 +9,11 @@ class Noticia(models.Model):
     dt_noticia = models.DateField(auto_now_add=True)
     subtitulo = models.CharField(max_length=255, null=True)
     texto = models.TextField(null=True)
-    foto = models.ForeignKey(
+    foto_noticia = models.ForeignKey(
         Image,
         related_name="+",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         default=None,
-    )
-
-    def __str__(self):
-        return self.nome_banda
+    )       
