77b56ec580eb3b70ceb6b8023d48d2496334e329
Anteconix <magnofilho2323@gmail.com>
2022-12-08 10:48:27 -0300

    subtituto fixx

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -3,7 +3,7 @@ from django.db import models
 class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
     dt_noticia = models.DateField(auto_now_add=True)
-    subtitulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255, null=True)
     texto = models.TextField()
 
     def __str__(self):
