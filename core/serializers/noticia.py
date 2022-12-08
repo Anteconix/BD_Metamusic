0bb42f24ec65f2da9b3cfcd960ae2fbb4c03696c
Anteconix <magnofilho2323@gmail.com>
2022-12-08 13:04:05 -0300

    image serializer

--- core/serializers/noticia.py
+++ core/serializers/noticia.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from rest_framework.serializers import ModelSerializer, SlugRelatedField, ImageSerializer
 from core.models import Noticia
 from media.models import Image
 
