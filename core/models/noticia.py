3f1ea29b87439d3c08f8f2feea189a682cde0661
Wecreds <wecredsbr@gmail.com>
2022-12-08 10:10:28 -0300

    charfield noticias

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -3,12 +3,12 @@ from django.db import models
 class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
     dt_noticia = models.DateField(auto_now_add=True)
-    subtitulo = models.TextField(max_length=255)
-    corpo1 = models.TextField(max_length=255)
-    corpo2 = models.TextField(max_length=255)
-    corpo3 = models.TextField(max_length=255)
-    corpo4 = models.TextField(max_length=255)
-    corpo5 = models.TextField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    corpo1 = models.CharField(max_length=255)
+    corpo2 = models.CharField(max_length=255)
+    corpo3 = models.CharField(max_length=255)
+    corpo4 = models.CharField(max_length=255)
+    corpo5 = models.CharField(max_length=255)
 
     def __str__(self):
         return self.titulo_noticia
