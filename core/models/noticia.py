1778deef6f3b64c99aef160a1c8a42815d24ee2e
Anteconix <magnofilho2323@gmail.com>
2022-12-08 10:51:29 -0300

    texto fix

--- core/models/noticia.py
+++ core/models/noticia.py
@@ -4,7 +4,7 @@ class Noticia(models.Model):
     titulo_noticia = models.CharField(max_length=255)
     dt_noticia = models.DateField(auto_now_add=True)
     subtitulo = models.CharField(max_length=255, null=True)
-    texto = models.TextField()
+    texto = models.TextField(null=True)
 
     def __str__(self):
         return self.titulo_noticia
